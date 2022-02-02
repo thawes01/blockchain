f1b3e8f57dee821caa7a4c84f99d769078a6d5b5
TH <>
2022-02-02 10:07:51 +0000

    Add documentation

--- app/src/main/java/Blockchain/Block.java
+++ app/src/main/java/Blockchain/Block.java
@@ -2,8 +2,21 @@ package Blockchain;
 
 public class Block {
 
-    public Block(PrimaryBlockData data) {}
+    /**
+     * Allocates a {@code Block} in a blockchain.
+     *
+     * @param primaryData  the data contained within the block, from which the
+     *                     block's hash is generated
+     */
+    public Block(PrimaryBlockData primaryData) {}
 
+    /**
+     * Returns the hash of the block.
+     *
+     * Note: currently returns a default hash of "0".
+     *
+     * @return  the hash of the block.
+     */
     public String getHash() {
         return "0";
     }
--- app/src/main/java/Blockchain/StopClock.java
+++ app/src/main/java/Blockchain/StopClock.java
@@ -5,14 +5,38 @@ import java.time.Clock;
 public class StopClock {
     private final Clock clock;
 
+    /**
+     * Returns a {@code StopClock} based on the system's clock.
+     *
+     * Uses the UTC timezone. A wrapper around {@link Clock#systemUTC()}.
+     *
+     * @return  a {@code StopClock} based on the system's clock, in the UTC
+     *          timezone
+     */
     public static StopClock systemUTC() {
         return new StopClock(Clock.systemUTC());
     }
 
+    /**
+     * Allocates a {@code StopClock} object for timing computations.
+     *
+     * A wrapper around {@link Clock}.
+     *
+     * @param clock  a clock to use as the basis for timing
+     */
     public StopClock(Clock clock) {
         this.clock = clock;
     }
 
+    /**
+     * Returns the current time.
+     *
+     * The current time is represented as a number of milliseconds from the
+     * start of the epoch, as returned by {@link Clock#millis()} for the
+     * {@code Clock} supplied at instantiation this object.
+     *
+     * @return  the current number of milliseconds from the start of the epoch
+     */
     public long now() {
         return clock.millis();
     }
