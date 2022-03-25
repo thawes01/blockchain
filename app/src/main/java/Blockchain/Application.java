a2b22d7f819cff7a4ca1cfa8162cce90105d4538
TH <>
2022-03-25 12:30:04 +0000

    Separate configuration validation from data extraction

--- app/src/main/java/Blockchain/Application.java
+++ app/src/main/java/Blockchain/Application.java
@@ -5,31 +5,34 @@ public class Application {
     private final BlockchainGenerator blockchainGenerator;
     private final Printer printer;
 
+    private static void validateConfiguration(Configuration configuration) {
+        validateLengthOfBlockchain(configuration);
+        validateBlockchainGenerator(configuration);
+        validatePrinter(configuration);
+    }
+
     private static void validateBlockchain(Blockchain blockchain) {
         if (!blockchain.validate()) {
             throw new RuntimeException("Blockchain generated not valid, there is a problem with blockchain generation");
         }
     }
 
-    private static int getLengthOfBlockchainIfNotNegative(Configuration configuration) {
+    private static void validateLengthOfBlockchain(Configuration configuration) {
         if (configuration.lengthOfBlockchain < 0) {
             throw new RuntimeException("Length of blockchain negative, must be non-negative");
         }
-        return configuration.lengthOfBlockchain;
     }
 
-    private static BlockchainGenerator getBlockchainGeneratorIfNotNull(Configuration configuration) {
+    private static void validateBlockchainGenerator(Configuration configuration) {
         if (configuration.blockchainGenerator == null) {
             throw new RuntimeException("Blockchain generator is null, must be non-null");
         }
-        return configuration.blockchainGenerator;
     }
 
-    private static Printer getPrinterIfNotNull(Configuration configuration) {
+    private static void validatePrinter(Configuration configuration) {
         if (configuration.printer == null) {
             throw new RuntimeException("Printer is null, must be non-null");
         }
-        return configuration.printer;
     }
 
     /**
@@ -38,9 +41,10 @@ public class Application {
      * @param configuration  the configuration to run the application with
      */
     public Application(Configuration configuration) {
-        this.lengthOfBlockchain = getLengthOfBlockchainIfNotNegative(configuration);
-        this.blockchainGenerator = getBlockchainGeneratorIfNotNull(configuration);
-        this.printer = getPrinterIfNotNull(configuration);
+        validateConfiguration(configuration);
+        this.lengthOfBlockchain = configuration.lengthOfBlockchain;
+        this.blockchainGenerator = configuration.blockchainGenerator;
+        this.printer = configuration.printer;
     }
 
     /**
